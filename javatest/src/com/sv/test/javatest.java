a7004d88dbcf68fa073e845e5a5c658053694a10
SHUBHAM <SHUBHAM@192.168.0.4>
2018-06-11 13:19:47 +0530

    commite

--- javatest/src/com/sv/test/javatest.java
+++ javatest/src/com/sv/test/javatest.java
@@ -1,10 +1,21 @@
 package com.sv.test;
 
 public class javatest {
+<<<<<<< master
 	public static void main(String[] args) {
 		System.out.println("shubhu");
 
 		System.out.println("hi again");
 		System.out.println("hhh ss vvss");
 	}
+=======
+public static void main(String[] args) {
+	System.out.println("shubhu");
+	
+	System.out.println("hi again");
+	System.out.println("hhh ss vvss");
+	System.out.println("svsvsvsv umak");
+	//weloce
+}
+>>>>>>> 4da534e update dev2
 }
