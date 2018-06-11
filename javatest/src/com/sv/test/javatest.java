55d89ff918a67f2cd0a154e814aa7d0a395edf5d
SHUBHAM <SHUBHAM@192.168.0.4>
2018-06-11 13:18:42 +0530

    done,,,

--- javatest/src/com/sv/test/javatest.java
+++ javatest/src/com/sv/test/javatest.java
@@ -1,10 +1,10 @@
 package com.sv.test;
 
 public class javatest {
-public static void main(String[] args) {
-	System.out.println("shubhu");
-	
-	System.out.println("hi again");
-	System.out.println("hhh ss vvss");
-}
+	public static void main(String[] args) {
+		System.out.println("shubhu");
+
+		System.out.println("hi again");
+		System.out.println("hhh ss vvss");
+	}
 }
