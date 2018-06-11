2a9ea7cc36df8423ba6d4984c91b132add8711af
SHUBHAM <SHUBHAM@192.168.0.4>
2018-06-11 13:53:29 +0530

    commite

--- javatest/src/com/sv/test/javatest.java
+++ javatest/src/com/sv/test/javatest.java
@@ -7,7 +7,11 @@ public static void main(String[] args) {
 	System.out.println("hi again");
 	System.out.println("hhh ss vvss");
 	
+<<<<<<< HEAD
 	System.out.println("hhhhiiiiiiiii");
 /*	sou slash*/
+=======
+	/*System.out.println("hhhhiiiiiiiii")*/
+>>>>>>> stash
 }
 }
