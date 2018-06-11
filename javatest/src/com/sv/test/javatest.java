0be46fb022c4c72d324ccd6877afbcc5922104f6
SHUBHAM <SHUBHAM@192.168.0.4>
2018-06-11 13:29:03 +0530

    done slash

--- javatest/src/com/sv/test/javatest.java
+++ javatest/src/com/sv/test/javatest.java
@@ -1,21 +1,12 @@
 package com.sv.test;
 
 public class javatest {
-<<<<<<< master
-	public static void main(String[] args) {
-		System.out.println("shubhu");
-
-		System.out.println("hi again");
-		System.out.println("hhh ss vvss");
-	}
-=======
 public static void main(String[] args) {
 	System.out.println("shubhu");
 	
 	System.out.println("hi again");
 	System.out.println("hhh ss vvss");
-	System.out.println("svsvsvsv umak");
-	//weloce
+	
+	System.out.println("hhhhiiiiiiiii");
 }
->>>>>>> 4da534e update dev2
 }
